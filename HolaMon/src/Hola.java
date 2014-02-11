bbc26f41962a70130b83a144ef821c9d1d79a3d2
Alumno14 <Alumno14@SBS-ALUMNO14>
2014-02-11 09:55:56 +0100

    Prova

---
+++ HolaMon/src/Hola.java
@@ -0,0 +1,14 @@
+
+public class Hola {
+
+	public Hola() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hola món");
+
+	}
+
+}
