93d638c946081e999a55b5f12378d9646238c160
Alumno14 <Alumno14@SBS-ALUMNO14>
2014-02-11 09:57:15 +0100

    Hola

--- HolaMon/src/Hola.java
+++ HolaMon/src/Hola.java
@@ -5,6 +5,7 @@ public class Hola {
 		// TODO Auto-generated constructor stub
 	}
 
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hola món");
